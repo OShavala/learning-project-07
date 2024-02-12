f36f93ed9d918a677ff6aa7344870b9fbcc7d448
OShavala <olena.shvl@gmail.com>
2024-02-12 09:16:01 +0200

    second fixation

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        document.querySelector('.word-container').style.animationDelay = '2s';
+    }, 1000);
+});
+
